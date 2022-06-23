d6121566305e152e9fcfe696014af244360d0de3
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-06-23 14:32:11 +0100

    cleaning up initialisation

--- src/cli.ts
+++ src/cli.ts
@@ -6,23 +6,27 @@ import { validate } from './validate'
 const program = new Command();
 
 program
-  .option('-v, --validate', 'Validate Repository')
+  .name('assets-helper')
+  .description('CLI to automate the workflows on the map3.xyz assets repo')
   .parse(process.argv)
 
-program.parse(process.argv);
-const options = program.opts();
-
-
-if (options.validate) {
-  validate()
+program.command('validate')
+  .description('Validate that the assets repository is in a valid state')
+  .option('-n --network <string>', 'specific network to validate')
+  .action((options) => {
+    validate(options.network)
     .then(result => {
       if (result.valid) {
         console.log('Repository is valid');
+        process.exit(0);
       } else {
         console.log(`Repository is invalid. Errors:\n ${result.errors.join('\n')}`);
+        process.exit(1);
       }
-
     }).catch(err => { 
-        console.error("Error running validate", err)
+        console.error("Error running validate", err);
+        process.exit(1);
     });
-}
+});
+
+program.parse();
