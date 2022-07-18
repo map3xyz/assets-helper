81e8debcca4d72192bb7ee58ae196406c46b2422
Amadeo Pellicce <pellicceama@gmail.com>
2022-07-18 18:03:25 +0200

    add support for P2P URLs

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,15 +2,25 @@ import fs from 'fs'
 import path from 'path';
 import axios from 'axios';
 
+function getP2PUrl(hash) {
+  // example
+  // https://ipfs.io/ipfs/QmaznB5PRhMC696u8yZuzN6Uwrnp7Zmfa5CydVUMvLJc9i/aUSDC.svg#x-ipfs-companion-no-redirect
+  return `https://ipfs.io/ipfs/${hash}#x-ipfs-companion-no-redirect`;
+}
+
 export async function downloadFile (fileUrl: string, fileDestination: string, fileName: string): Promise<void> {
 
   return new Promise(async (resolve, reject) => {
     const localFilePath = path.resolve(fileDestination, fileName);
 
+    const isP2P = fileUrl.includes('ipfs://') || fileUrl.includes('ipns://');
+
+    const url = isP2P ? getP2PUrl(fileUrl): fileUrl;
+
     try {
         const response = await axios({
             method: 'GET',
-            url: fileUrl,
+            url: url,
             responseType: 'stream',
         });
 
