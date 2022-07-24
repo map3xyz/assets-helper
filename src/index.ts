5d4bfa4da4ea2820d2c7b5f219b8be0c6392b73c
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-07-24 15:29:23 +0100

    cleaning up index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { RepoFileGenerator } from './csv/RepoFileGenerator';
-import { DEFAULT_TEMP_DIR } from './utils/config';
-
 export * from './model';
 export * from './networks';
 export * from './repo';
@@ -9,18 +6,3 @@ export * from './utils';
 export * from './validate';
 export * from './db';
 export * from './csv';
-
-
-// // TODO remove temp async IIFE
-// (async () => {
-//     try {
-//         const csv = await RepoFileGenerator.generate();
-
-//         csv.deserialise(DEFAULT_TEMP_DIR);
-//         console.log('success')
-//     } catch (err) {
-//          console.error(err);
-//     }
-//   })();
-
-  
