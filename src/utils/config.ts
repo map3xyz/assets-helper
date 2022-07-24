630e14a00e62ddad376aa7141571b341b6d827f9
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2022-07-24 17:24:39 +0100

    increase cache for repo to 60m

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,3 +6,4 @@ export const DEFAULT_REPO_DISK_LOCATION = './tmp/map3xyz-assets';
 export const DEFAULT_TEMP_DIR = './tmp';
 export const DEFAULT_TWA_DISK_LOCATION = './tmp/trustwallet-assets';
 export const ASSETS_CSV_TMP_FILE = './tmp/assets.csv';
+export const ASSETS_REPO_CACHE_MINUTES = 60; // 60 minutes
